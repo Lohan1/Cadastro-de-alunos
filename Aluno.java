8266b604892378528490df7a416af754c581b9b0
Lohan Silva <133441351+Lohan1@users.noreply.github.com>
2023-05-16 20:15:39 -0300

    Update Aluno.java

--- Aluno.java
+++ Aluno.java
@@ -1,40 +1,76 @@
-public class Aluno {
 
-    public String nome, sexo, endereco, cidade, estado, nomeDoPai, nomeDaMae;  
+import java.util.Scanner;
 
-    public int CEP, RG, grauDeEscolaridade, dataDeNascimento, telefone; 
-    
-    
-    public static void main(String[] args) {
-        Aluno aluno1 = new Aluno();
-        aluno1.nome = "André Martins";
-        aluno1.sexo = "Masculino";
-        aluno1.endereco = "Rua Santo Antonio, 29 - Centro";
-        aluno1.cidade = "São Paulo";
-        aluno1.estado = "SP";
-        aluno1.nomeDoPai = "João Martins";
-        aluno1.nomeDaMae = "Maria Martins";
-        aluno1.telefone = 2344565;
-        aluno1.CEP = 102456827;
-        aluno1.RG = 345644334;
-        aluno1.dataDeNascimento = 71281;
-        aluno1.grauDeEscolaridade = 2;
+public class aluno {
+    private String nome, sexo, endereco,
+            cidade, estado, nomeDoPai, nomeDaMae;
+    private int cep, telefone, rg,
+            dataDeNascimento, grauDeEscolaridade;
+
+    public static void cadastraraluno(aluno aluno, Scanner scanner) {
+        System.out.println("Entre com o nome:");
+        aluno.nome = scanner.next();
+
+        System.out.println("Entre com o sexo:");
+        aluno.sexo = scanner.next();
+
+        System.out.println("Entre com o endereço:");
+        aluno.endereco = scanner.next();
+
+        System.out.println("Entre com a cidade onde o aluno reside:");
+        aluno.cidade = scanner.next();
+
+        System.out.println("Entre com a sigla do estado onde o aluno reside:");
+        aluno.estado = scanner.next();
+
+        System.out.println("Entre com o CEP (somente números):");
+        aluno.cep = scanner.nextInt();
+
+        System.out.println("Entre com o telefone (somente números):");
+        aluno.telefone = scanner.nextInt();
+
+        System.out.println("Entre com o nome do pai do aluno:");
+        aluno.nomeDoPai = scanner.next();
+
+        System.out.println("Entre com o nome da mãe do aluno:");
+        aluno.nomeDaMae = scanner.next();
+
+        System.out.println("Entre com o Registro Geral (RG) do aluno (somente números):");
+        aluno.rg = scanner.nextInt();
+
+        System.out.println("Entre com o grau de escolaridade do aluno (0, 1, 2, 3):");
+        aluno.grauDeEscolaridade = scanner.nextInt();
         
-        System.out.println("nome do aluno; " + aluno1.nome);
-        System.out.println("sexo do aluno; " + aluno1.sexo);
-        System.out.println("endereco do aluno; " + aluno1.endereco);
-        System.out.println("cidade do aluno; " + aluno1.cidade);
-        System.out.println("estado do aluno; " + aluno1.estado);
-        System.out.println("nome do pai do aluno; " + aluno1.nomeDoPai);
-        System.out.println("nome da mae do aluno; " + aluno1.nomeDaMae);
-        System.out.println("telefone do aluno; " + aluno1.telefone);
-        System.out.println("cep do aluno; " + aluno1.CEP);
-        System.out.println("rg do aluno; " + aluno1.RG);
-        System.out.println("data de nascimento do aluno; " + aluno1.dataDeNascimento);
-        System.out.println("grau de escolaidade do aluno; " + aluno1.grauDeEscolaridade);
-        
-        
+        System.out.println("Entre com a data de nascimento do aluno (somente números):");
+        aluno.dataDeNascimento = scanner.nextInt();
     }
 
-
+    public static void exibirDadosaluno(aluno aluno) {
+        System.out.println("Dados do Aluno:");
+        System.out.println("aluno: " + aluno.nome);
+        System.out.println("Sexo: " + aluno.sexo);
+        System.out.println("Registro Geral (RG): " + aluno.rg);
+        System.out.println("Grau de escolaridade: " + aluno.grauDeEscolaridade);
+        System.out.println("Endereço: " + aluno.endereco + ", Cidade: " + aluno.cidade + ", Estado: " + aluno.estado);
+        System.out.println("CEP: " + aluno.cep);
+        System.out.println("Telefone: " + aluno.telefone);
+        System.out.println("Nome do pai: " + aluno.nomeDoPai);
+        System.out.println("Nome da mãe: " + aluno.nomeDaMae);
+        System.out.println("Data de nascimento: " + aluno.dataDeNascimento);
+    }
+    
+    public static void main(String[] args) {
+        aluno aluno = new aluno();
+        Scanner scanner = new Scanner(System.in);
+        for (int i = 1; i <= 50; i++) {
+            System.out.println("Entre com os dados do aluno " + i + ":");
+            cadastraraluno(aluno, scanner);
+            exibirDadosaluno(aluno);
+            System.out.println("Quer continuar? (s ou n)");
+            String resposta = scanner.next();
+            if (resposta.contentEquals("n")) {
+                i = 51;
+            }
+        }
+    }
 }
